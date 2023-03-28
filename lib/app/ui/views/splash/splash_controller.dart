a37abcbc33cbb8822cbb8d6d23061ffc98c28af8
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2023-03-28 18:51:48 -0500

    wip: :construction_worker: PILAR-013 add cronortype home quizz view

--- lib/app/ui/views/splash/splash_controller.dart
+++ lib/app/ui/views/splash/splash_controller.dart
@@ -7,7 +7,7 @@ class SplashController extends GetxController {
     Future.delayed(const Duration(seconds: 5), () {
       Get.offNamed(AppRoutes.login);
       // Get.offNamed(AppRoutes.home);
-      Get.toNamed(AppRoutes.home);
+      Get.toNamed(AppRoutes.quizzHome);
     });
     super.onReady();
   }
